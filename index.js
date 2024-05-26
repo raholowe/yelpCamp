c91df0855fcd27efed48b4546414c941e83650ca
raholowe <raholowe@gmail.com>
2024-05-26 16:15:16 -0600

    Added helmet to secure app by setting HTTP headers. configured content-security-policy to allow content from certain sites

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app= express();
 const path= require('path');
 const mongoose = require('mongoose');
 const session = require('express-session')
+const MongoStore = require('connect-mongo');
 const flash= require('connect-flash')
 const ejsMate= require('ejs-mate')
 const campGround= require('./models/campground')
@@ -23,25 +24,92 @@ const passport= require('passport')
 const LocalPassport= require('passport-local')
 const User= require('./models/user')
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet=require('helmet')
+//const dbUrl=process.env.DB_URL
+const dbUrl='mongodb://localhost:27017/yelpcamp'
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 //app.use(express.json({ extended: true }));
 app.use(mongoSanitize());
 
+const store=MongoStore.create({
+  mongoUrl: dbUrl,
+  secret:'tempsecret',
+  touchAfter: 24 * 3600
+})
+store.on('error', function(e){
+  console.log('error storing session',e)
+})
+
 
 const sessionConfig ={
+  store,
+  name:'session',
   secret: 'tempSecret',
   resave:false,
   saveUninitialized:true,
   cookie:{
     httpOnly:true,
+    //secure:true,
     expires: Date.now() +1000*60*60*24*7,
     maxAge:1000*60*60*24*7
   }
 }
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet())
+
+const scriptSrcUrls = [
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://api.mapbox.com/",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+  "https://cdn.jsdelivr.net",
+]
+const styleSrcUrls = [
+  "https://kit-free.fontawesome.com/",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.mapbox.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+  "https://cdn.jsdelivr.net",
+]
+const connectSrcUrls = [
+  "https://api.mapbox.com/",
+  "https://a.tiles.mapbox.com/",
+  "https://b.tiles.mapbox.com/",
+  "https://events.mapbox.com/",
+]
+const fontSrcUrls = []
+app.use(
+  helmet.contentSecurityPolicy({
+      directives: {
+          defaultSrc: [],
+          connectSrc: ["'self'", ...connectSrcUrls],
+          scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+          styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+          workerSrc: ["'self'", "blob:"],
+          objectSrc: [],
+          imgSrc: [
+              "'self'",
+              "blob:",
+              "data:",
+              "https://res.cloudinary.com/dzpdeyqbr/",
+              "https://images.unsplash.com/",
+          ],
+          fontSrc: ["'self'", ...fontSrcUrls],
+      },
+  })
+)
+
+
+
+
+
+
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalPassport(User.authenticate()));
@@ -66,7 +134,7 @@ app.set('views',path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 //connect to mongoose
-mongoose.connect('mongodb://localhost:27017/yelpcamp')
+mongoose.connect(dbUrl)
 const db= mongoose.connection;
 
 //callback for if ther is an errror
@@ -77,12 +145,6 @@ db.once("open", ()=>{
   console.log("Database Connected")
 })
 
-//just a fake user creator
-app.get('/fakeuser', async (req,res)=>{
-  const user= new User({email: 'test@test.com', username:'test'})
-  const newuser= await User.register(user, 'password')
-  res.send(newuser)
-})
 
 //test Connection
 app.get('/',(req,res)=>{
