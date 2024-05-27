24d07ba08bf71f4f1a4a25639205ee00b8ba40fc
raholowe <raholowe@gmail.com>
2024-05-26 18:09:51 -0600

    change port to listen for render default port

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ app.use((err,req,res,next)=>{
 })
 
 
-
-app.listen(3000,()=>{
-  console.log('Listening on port 3000')
+const port = process.env.PORT || 3000;
+app.listen(port,()=>{
+  console.log(`Listening on port ${port}`)
 })
